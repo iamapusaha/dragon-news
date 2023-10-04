6b0a2ff9bc14e96739ff460453fb7689f1eddba3
iamapusaha <apusaha392@gmail.com>
2023-10-04 20:34:20 +0600

    added header

--- src/layouts/Root.jsx
+++ src/layouts/Root.jsx
@@ -4,7 +4,6 @@ import { Outlet } from "react-router-dom";
 const Root = () => {
     return (
         <div className="container mx-auto font-poppins">
-            <h1>this is root page</h1>
             <Outlet></Outlet>
         </div>
     );
--- src/pages/shared/Header/Header.jsx
+++ src/pages/shared/Header/Header.jsx
@@ -1,9 +1,14 @@
-
+import logo from '../../../assets/logo.png'
+import moment from 'moment';
 
 const Header = () => {
     return (
-        <div>
-            <h1>this is header</h1>
+        <div className='text-center my-5'>
+            <div className='flex justify-center'>
+                <img className='mb-5' src={logo} alt="" />
+            </div>
+            <p className='text-lg font-normal mb-2 text-[#706F6F]'>Journalism Without Fear or Favour</p>
+            <p className='text-xl font-medium text-[#706F6F]'>{moment().format("dddd, MMMM D, YYYY")}</p>
         </div>
     );
 };
