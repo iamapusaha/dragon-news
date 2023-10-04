83190a8a734b2e28b98a8d353ec1e77b9ba8552b
iamapusaha <apusaha392@gmail.com>
2023-10-04 20:08:29 +0600

    add custom font for tailwind

--- src/layouts/Root.jsx
+++ src/layouts/Root.jsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router-dom";
 
 const Root = () => {
     return (
-        <div>
+        <div className="container mx-auto font-poppins">
             <h1>this is root page</h1>
             <Outlet></Outlet>
         </div>
