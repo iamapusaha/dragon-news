40f5ade458f672102b919552bd0b036efe28c2e5
iamapusaha <apusaha392@gmail.com>
2023-10-05 00:23:46 +0600

    added navbar

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,7 @@
 import Header from "../shared/Header/Header";
+import LeftSideNav from "../shared/LeftSideNav/LeftSideNav";
 import NavBar from "../shared/NavBar/NavBar";
+import RightSideNav from "../shared/RightSideNav/RightSideNav";
 
 
 const Home = () => {
@@ -7,6 +9,17 @@ const Home = () => {
         <div>
             <Header></Header>
             <NavBar></NavBar>
+            <div className="grid grid-cols-1 md:grid-cols-4 gap-6">
+                <div>
+                    <LeftSideNav></LeftSideNav>
+                </div>
+                <div className="md:col-span-2">
+                    <h1>comming soon......................</h1>
+                </div>
+                <div>
+                    <RightSideNav></RightSideNav>
+                </div>
+            </div>
         </div>
     );
 };
