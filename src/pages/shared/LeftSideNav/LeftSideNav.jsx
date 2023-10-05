b4bb22dce5d5e661a94703c8dedb1e34c19d0fbd
iamapusaha <apusaha392@gmail.com>
2023-10-05 13:51:08 +0600

    added all category

--- src/pages/shared/LeftSideNav/LeftSideNav.jsx
+++ src/pages/shared/LeftSideNav/LeftSideNav.jsx
@@ -1,9 +1,26 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 
 const LeftSideNav = () => {
+    const [categories, setCategories] = useState([])
+    useEffect(() => {
+        fetch('categories.json')
+            .then(res => res.json())
+            .then(data => setCategories(data))
+    }, [])
     return (
         <div>
-            <h1>this is left side nav</h1>
+            <div className="">
+                <h1 className="text-xl font-semibold">All Caterogy</h1>
+                <ul className="menu menu-vertical text-xl font-medium">
+                    {
+                        categories.map(category =>
+                            <li className="px-3" key={category.id}><Link to={`/category/${category.name}`}>{category.name}</Link></li>
+                        )
+                    }
+                </ul>
+            </div>
         </div>
     );
 };
