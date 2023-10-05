0b083a784c909d35381efd2eeffbce2849c02201
iamapusaha <apusaha392@gmail.com>
2023-10-05 11:04:56 +0600

    added design login bar with social media

--- src/pages/shared/RightSideNav/RightSideNav.jsx
+++ src/pages/shared/RightSideNav/RightSideNav.jsx
@@ -1,9 +1,19 @@
-
+import { FaGithub, FaGoogle } from 'react-icons/fa';
 
 const RightSideNav = () => {
     return (
         <div>
-            <h1>this is right side nav</h1>
+            <div>
+                <h1 className="text-xl font-semibold mb-3">Login with</h1>
+                <button className='flex gap-2 rounded-lg items-center border p-2 w-full justify-center mb-2'>
+                    <FaGoogle></FaGoogle>
+                    Login with Google
+                </button>
+                <button className='flex gap-2 rounded-lg items-center border p-2 w-full justify-center'>
+                    <FaGithub></FaGithub>
+                    Login with GitHub
+                </button>
+            </div>
         </div>
     );
 };
