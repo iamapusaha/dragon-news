15ffc43064f8f0d37d2d49f032f7485816fa468d
iamapusaha <apusaha392@gmail.com>
2023-10-05 11:19:55 +0600

    added find us on

--- src/pages/shared/RightSideNav/RightSideNav.jsx
+++ src/pages/shared/RightSideNav/RightSideNav.jsx
@@ -1,11 +1,12 @@
-import { FaGithub, FaGoogle } from 'react-icons/fa';
+import { FaFacebook, FaGithub, FaGoogle, FaInstagram } from 'react-icons/fa';
+import { FaXTwitter } from "react-icons/fa6";
 
 const RightSideNav = () => {
     return (
         <div>
-            <div>
+            <div className='mb-8'>
                 <h1 className="text-xl font-semibold mb-3">Login with</h1>
-                <button className='flex gap-2 rounded-lg items-center border p-2 w-full justify-center mb-2'>
+                <button className='flex gap-2 rounded items-center border p-2 w-full justify-center mb-2'>
                     <FaGoogle></FaGoogle>
                     Login with Google
                 </button>
@@ -13,6 +14,22 @@ const RightSideNav = () => {
                     <FaGithub></FaGithub>
                     Login with GitHub
                 </button>
+            </div>
+            <div>
+                <h1 className="text-xl font-semibold mb-3">Find Us On</h1>
+                <a href='#' className='flex gap-2 rounded-t items-center border p-4 w-full'>
+                    <FaFacebook></FaFacebook>
+                    Facebook
+                </a>
+                <a href='#' className='flex gap-2 items-center border-x p-4 w-full'>
+                    <FaXTwitter></FaXTwitter>
+                    XTwitter
+                </a>
+                <a href='#' className='flex gap-2 items-center rounded-b border p-4 w-full'>
+                    <FaInstagram></FaInstagram>
+                    Instragram
+                </a>
+
             </div>
         </div>
     );
